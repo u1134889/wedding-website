60bb5d9fde2ab06bf0c9513bcda6933a5a67a1a3
Justin R <u1134889@umail.utah.edu>
2023-02-19 18:06:53 -0700

    Adds mobile friendliness

--- pages/save-the-date.tsx
+++ pages/save-the-date.tsx
@@ -12,7 +12,7 @@ export default function SaveTheDate() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="py-4">
+        <div className="py-8">
           <Image
             src={sitting}
             alt="Venisse and Justin sitting together in Utah mountains"
@@ -20,40 +20,33 @@ export default function SaveTheDate() {
           />
         </div>
         <div className="relative my-4">
-          <div className="bg-gradient-to-b from-black absolute top-0 left-0 right-0">
-            <h1 className="font-bold text-6xl text-center">Save The Date</h1>
-            <h1 className="font-bold text-6xl text-center">Venisse and Justin</h1>
-            <p className="text-lg text-center">Saturday, 4th of May 2024</p>
-            <p className="text-lg text-center">Milwaukee, Wisconsin</p>
-            <div className="py-20"></div>
+          <div className="xl:absolute xl:top-0 xl:left-0 xl:right-0">
+            <div className="xl:bg-[rgba(0,0,0,0.75)]">
+              <h1 className="font-bold text-6xl text-center">Save The Date</h1>
+              <h1 className="font-bold text-6xl text-center">Venisse and Justin</h1>
+              <p className="text-lg text-center">Saturday, 4th of May 2024</p>
+              <p className="text-lg text-center">Milwaukee, Wisconsin</p>
+            </div>
+            <div className="py-4 xl:py-32 xl:bg-gradient-to-b xl:from-[rgba(0,0,0,0.75)]"></div>
           </div>
           <Image
             src={holding}
             alt="Venisse and Justin holding each other in Utah mountains"
             placeholder="blur"
           />
-          <div className="bg-gradient-to-t from-black absolute bottom-0 left-0 right-0">
-            <div className="py-20"></div>
-            <p className="text-lg text-center">Invitation to follow</p>
-            <p className="text-lg text-center">Please enter your contact and mailing address below</p>
-            <div className="my-8 flex justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Centered Button
-              </button>
+          <div className="xl:absolute xl:bottom-0 xl:left-0 xl:right-0">
+            <div className="py-4 xl:py-32 xl:bg-gradient-to-t xl:from-[rgba(0,0,0,0.75)]"></div>
+            <div className="xl:bg-[rgba(0,0,0,0.75)]">
+              <p className="text-lg text-center">Invitation to follow</p>
+              <p className="text-lg text-center">Please enter your contact and mailing address below</p>
+              <div className="py-8 flex justify-center">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Centered Button
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        <h1>
-          Sitjar-Rogosienski Wedding 2024
-        </h1>
-
-        <h3>When</h3>
-        <p>May 4th, 2024</p>
-
-        <h3>Where</h3>
-        <p><a href="https://goo.gl/maps/UbcXZnsv7RJ9Gjsc9" target="_blank" rel="noopener noreferrer">South Shore Pavilion, Milwaukee, WI</a></p>
-
-
       </main>
     </div>
   );
